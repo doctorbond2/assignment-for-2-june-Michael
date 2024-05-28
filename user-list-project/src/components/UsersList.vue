65f35472333caad54022c22b9905e3c6ea33eccb
doctorbond2 <michael.rostlund@hotmail.com>
2024-05-29 00:32:16 +0200

    Basic fetch made, more practice with ref and other "hooks" coming up. Then layout.

--- user-list-project/src/components/UsersList.vue
+++ user-list-project/src/components/UsersList.vue
@@ -1,15 +1,32 @@
 <script setup>
-import { ref } from 'vue';
-const timer = ref(0);
-const interval = setInterval(() => {
-  timer.value++;
-  if (timer.value >= 10) {
-    timer.value = 0;
-    clearInterval(interval);
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import API_Handler from '@/utils/apiHandler.js';
+const userListData = ref(null);
+const fetchData = async () => {
+  try {
+    const response = await API_Handler.fetchData();
+    userListData.value = response.data;
+    console.log(userListData.value.data);
+  } catch (err) {
+    console.log(err.error, err.response);
   }
-}, 1000);
+};
+onMounted(() => {
+  fetchData();
+});
 </script>
 <template>
-  <div>{{ timer }}</div>
+  <div>
+    <!-- <button @click="fetchData">Get data</button> -->
+
+    <div v-if="userListData">
+      Data received.
+      <div v-for="user in userListData.data">
+        <h3>{{ user.first_name }}</h3>
+      </div>
+    </div>
+    <div v-else>No data received yet</div>
+  </div>
 </template>
 <style scoped></style>
---
+++ user-list-project/src/utils/apiHandler.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const reqres_instance = axios.create({
+  baseURL: 'https://reqres.in/api/users',
+  timeout: 6000,
+});
+class APIHandler {
+  constructor() {}
+
+  async fetchData() {
+    try {
+      const response = await reqres_instance.get();
+      return response;
+    } catch (err) {
+      throw { error: err, response: err.response };
+    }
+  }
+}
+
+const API_Handler = new APIHandler();
+export default API_Handler;
