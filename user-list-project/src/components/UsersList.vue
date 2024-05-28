84b30e1e793900e833a0ff68ca032390c0c29021
doctorbond2 <michael.rostlund@hotmail.com>
2024-05-28 22:34:42 +0200

    refactor: Update Vite configuration, remove unnecessary code, and add UsersList component

---
+++ user-list-project/src/components/UsersList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+const timer = ref(0);
+const interval = setInterval(() => {
+  timer.value++;
+  if (timer.value >= 10) {
+    timer.value = 0;
+    clearInterval(interval);
+  }
+}, 1000);
+</script>
+<template>
+  <div>{{ timer }}</div>
+</template>
+<style scoped></style>
--- user-list-project/src/views/HomeView.vue
+++ user-list-project/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import UsersList from '@/components/UsersList.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <UsersList />
   </main>
 </template>
