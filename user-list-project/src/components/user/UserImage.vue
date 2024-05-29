bc9dac50eaa1bba864caa5917ba195ad73170649
doctorbond2 <michael.rostlund@hotmail.com>
2024-05-29 23:10:36 +0200

    refactor: Improve user card layout and styling

--- user-list-project/src/components/user/UserImage.vue
+++ user-list-project/src/components/user/UserImage.vue
@@ -10,17 +10,20 @@ const imageClickHandler = () => {
 };
 </script>
 <template>
-  <div type="button" @click="imageClickHandler">
+  <div type="button" @click="imageClickHandler" class="image-wrapper">
     <img
       :src="image"
       alt="user-profile-picture"
-      class="card-profile-picture border border-slate-600"
+      class="card-profile-picture border border-slate-600 m-2"
     />
   </div>
 </template>
 <style scoped>
 .card-profile-picture {
   border-radius: 50%;
-  width: 10vw;
+  width: 8vw;
+}
+.image-wrapper {
+  width: fit-content;
 }
 </style>
--- user-list-project/src/components/user/UserListItem.vue
+++ user-list-project/src/components/user/UserListItem.vue
@@ -10,14 +10,20 @@ const { user } = props;
 const { first_name, last_name, avatar, email, id } = user;
 </script>
 <template>
-  <div class="user_card_wrap">
-    <p>{{ id }}</p>
-    <UserImage :image="avatar" />
-    <h1>{{ first_name }} {{ last_name }}</h1>
+  <div class="user_card_wrap grid grid-cols-2">
+    <section class="left-section" name="left-section">
+      <UserImage :image="avatar" />
+    </section>
+    <h2>{{ first_name }} {{ last_name }}</h2>
+    <section class="right-section" name="right-section"></section>
   </div>
 </template>
 <style scoped>
 .user_card_wrap {
-  @apply max-w-sm rounded overflow-hidden shadow-lg bg-slate-200 m-10 hover:-translate-y-5 transition-transform;
+  @apply max-w-64 rounded overflow-hidden shadow-md bg-slate-200 m-10 hover:-translate-y-5 transition-transform;
+}
+.left-section {
+  display: flex;
+  justify-content: center;
 }
 </style>
